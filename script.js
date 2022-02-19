b0153676e695a63f70f7683803697fee16f16b00
Qiao Zhang <zqthedesigner@gmail.com>
2022-02-20 01:07:08 +0800

    update

---
+++ script.js
@@ -0,0 +1,28 @@
+// This is a single line comment.
+
+/* 
+This is also a comment, but 
+can split in multiple lines
+ */
+
+/**
+ * This is same as the 2nd comment
+ * but formatted in a  nicer style
+ */
+
+/**
+ * Comment is important. The code tells computer what to do.
+ * The comment tells  you  and others what does the CODE do. 
+ */
+
+// This line will display the string "test" in console
+console.log('test')
+
+// If you comment a code, it will not run
+// console.log('This will NOT show in console')
+/**
+ * console.log('This will NOT show in console as well')
+ */
+
+// Try to UNCOMMENT the code below, and refresh the page
+// console.log('You should see this in the browser console')
